1fa0bc3b92b366d8490ef52f504e330e2d739e51
PercTruman <drshawnhart@gmail.com>
2022-06-07 11:43:12 -0500

    got movie page working

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  // const allGenres = movies.map((movie)=>movie.genres)
+  // const individualGenres = allGenres.map(group=><ul>{group}</ul>)
+  // const  genres   =individualGenres.map(genre=><li>{genre}</li>)
+
+  const movieList = movies.map((movie) => (
+    <div>
+      <h3>Name: {movie.title}</h3>
+      <h4>Time: {movie.time}</h4>
+       <h4>Genres:</h4>
+      <ul>{movie.genres.map(genre=><li>{genre}</li>)}</ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <div>{movieList}</div>
+    </div>
+  );
 }
 
 export default Movies;
